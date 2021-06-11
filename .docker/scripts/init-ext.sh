3589d577deb2c6918029f52605e2c9b9c76d6745
Carl Antuar <carl.antuar@smartservice.qld.gov.au>
2021-06-11 11:52:26 +1000

    [QOL-8092] fix WORKDIR references

--- .docker/scripts/init-ext.sh
+++ .docker/scripts/init-ext.sh
@@ -4,9 +4,6 @@
 #
 set -e
 
-cd $WORKDIR
-pwd
-ls -al
 pip install -r "requirements.txt"
 pip install -r "requirements-dev.txt"
 python setup.py develop
--- .docker/scripts/serve.sh
+++ .docker/scripts/serve.sh
@@ -6,8 +6,8 @@ dockerize -wait tcp://solr:8983 -timeout 1m
 
 sed -i "s@SITE_URL@${SITE_URL}@g" $CKAN_INI
 
-if [ -f "${APP_DIR}/bin/ckan" ]; then
-    ${APP_DIR}/bin/ckan -c ${CKAN_INI} run
+if (which ckan >/dev/null); then
+    ckan -c ${CKAN_INI} run
 else
-    ${APP_DIR}/bin/paster serve ${CKAN_INI}
+    paster serve ${CKAN_INI}
 fi
