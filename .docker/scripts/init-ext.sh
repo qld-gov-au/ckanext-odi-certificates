f2e8acc6229fe86ce2102e470b1eb920898513e1
Carl Antuar <carl.antuar@smartservice.qld.gov.au>
2021-06-11 11:31:10 +1000

    [QOL-8092] drop virtualenv reference as it no longer applies

--- .docker/scripts/init-ext.sh
+++ .docker/scripts/init-ext.sh
@@ -4,7 +4,6 @@
 #
 set -e
 
-. ${APP_DIR}/bin/activate
 cd $WORKDIR
 pip install -r "requirements.txt"
 pip install -r "requirements-dev.txt"
@@ -13,5 +12,3 @@ installed_name=$(grep '^\s*name=' setup.py |sed "s|[^']*'\([-a-zA-Z0-9]*\)'.*|\1
 
 # Validate that the extension was installed correctly.
 if ! pip list | grep "$installed_name" > /dev/null; then echo "Unable to find the extension in the list"; exit 1; fi
-
-deactivate
